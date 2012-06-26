11b9634d0adfbc1c6243f0f0fb3ac4ebcf0e419d
David Padilla <david@crowdint.com>
2012-06-26 16:14:58 -0500

    Drama page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def show
+    @dramas = Drama.all_ordered
+    @days_without_drama = Drama.days_without_drama
   end
 end
--- app/models/drama.rb
+++ app/models/drama.rb
@@ -2,4 +2,12 @@ class Drama < ActiveRecord::Base
   attr_accessible :description
   belongs_to :creator, class_name: 'User'
 
+  def self.days_without_drama
+    last_drama = last.created_at || Time.now
+    ((Time.now - last_drama) / 1.day).to_i
+  end
+
+  def self.all_ordered
+    order('created_at DESC')
+  end
 end
