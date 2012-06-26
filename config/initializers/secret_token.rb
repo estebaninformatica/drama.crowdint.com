361dd93aa50af750873ba4ade0dec8adf79546c8
David Padilla <david@crowdint.com>
2012-06-26 15:13:03 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdrama::Application.config.secret_token = '97b7348d9c52fa10b81d37123a9ce45df3b6860e2734af9a53ee32ef01c03d48fe1a630e202937df765bc989fbb7d7764d8b16e9657387ef5c3fabcffc1c040f'
