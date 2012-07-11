7fe1293418f984747746f30d61053ad46e52d479
David Padilla <david@crowdint.com>
2012-07-11 12:39:32 -0500

    Fix a bug on the homepage where the wrong drama was being used to calculate days without drama

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     # Have to call all so I can get the last drama for the
     # second statement
     @dramas = Drama.all_ordered_and_published.all
-    @days_without_drama = @dramas.last ? Drama.days_without_drama(@dramas.last) : 365
+    @days_without_drama = @dramas.first ? Drama.days_without_drama(@dramas.first) : 365
   end
 end
