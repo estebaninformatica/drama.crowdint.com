0452209cc5e7c240d9fc71605a99da3d48660f43
David Padilla <david@crowdint.com>
2012-07-18 18:37:18 -0500

    Drama.days_without_drama specs

--- spec/models/drama_spec.rb
+++ spec/models/drama_spec.rb
@@ -36,4 +36,13 @@ describe Drama do
       end
     end
   end
+
+  context :days_without_drama do
+    it "returns the difference in days from last drama to today" do
+      last_drama = Drama.new
+      last_drama.drama_at = 5.days.ago
+
+      Drama.days_without_drama(last_drama).should eq(5)
+    end
+  end
 end
