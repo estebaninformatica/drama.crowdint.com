be70d4a7790d8b1d3770c9b2ef256aa7a9d12a88
David Padilla <david@crowdint.com>
2012-11-04 22:32:24 -0700

    Use a header for the AUTH_TOKEN if present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  prepend_before_filter :api_key
   before_filter :authenticate, :calculate_streak
 
   def authenticate
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   def calculate_streak
     @streak = Streak.best_streak
+  end
+
+  def api_key
+    if api_key = params[:token].blank? && request.headers["X-AUTH-TOKEN"]
+      params[:token] = api_key
+    end
   end
 end
