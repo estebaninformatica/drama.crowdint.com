21488a65c82996db95f980c43b0c23c2b9efbd19
Hector Bustillos <hector.bustillos@crowdint.com>
2012-12-05 09:54:44 -0600

    Add rabl view for dramas index

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -2,7 +2,7 @@ class DramasController < ApplicationController
   respond_to :json
 
   def index
-    @dramas = Drama.all_ordered_and_published
+    @dramas = Drama.all_ordered_and_published.all
     respond_with @dramas
   end
 end
---
+++ app/views/dramas/index.rabl
@@ -0,0 +1,2 @@
+collection :@dramas, :root => :dramas
+attributes :id, :description, :drama_at, :created_at
